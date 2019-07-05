f6fabdf0d040744da026f9a3c08da259f0487ef0
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-05 15:50:07 -0500

    Create shopping cart React shell

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import Cart from './Cart.jsx';
 
 class App extends React.Component {
   constructor () {
     super();
+
+    this.state = {
+      cart: [{ id: 0, name: 'Laptop', price: 100, picture: '' }],
+      display: true,
+    };
   }
   render () {
-    return (<h1>Fix me!</h1>);
+    return (
+      <div>
+        <ul>
+          <li>Hi! Sign in or register</li>
+          <li>Daily Deals</li>
+          <li>Gift Cards</li>
+          <li>Help & Contact</li>
+        </ul>
+        <ul>
+          <li>Sell</li>
+          <li>My SeaBay</li>
+          <li>Notifications</li>
+          <li>
+            <Cart cart={this.state.cart} display={this.state.display} />
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
