795454fff3cfbca0a066180aa3a6c75bf15b00c4
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-05 14:58:03 -0500

    Change HTML title

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(cors());
-app.use(express.static('client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('client/dist'));
 
 app.get('/', (req, res) => {
   res.status(200).send('Get request successful!');
