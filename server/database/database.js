f1847ceda99f1a727c6473ea838c00407c94e914
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-08 17:15:02 -0500

    Add mongoose connection

--- server/database/database.js
+++ server/database/database.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+const connection =  mongoose.connection;
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
+const ProductSchema = new Schema({
+  id: Number,
+  name: String,
+  price: Number,
+  category: String,
+  img1_url: String
+});
+
+const Products = mongoose.model('product', ProductSchema);
+
+Products.find({ category: 'test_category' }).then(console.log);
