99f4b287972d7c78dc90aa77d5b4a1a31670a866
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-08 16:50:11 -0500

    Add mongodb and mongoose

--- server/database/database.js
+++ server/database/database.js
@@ -1,16 +1,2 @@
-const mysql = require('mysql');
-const Promise = require('bluebird');
+const mongoose = require('mongoose');
 
-const connection = mysql.createConnection({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-const db = Promise.promisifyAll(connection, { multiArgs: true });
-
-db.connectAsync()
-.then(() => console.log('Database connected'))
-.catch(() => console.log('Connection failed'));
-
-module.exports = db;
