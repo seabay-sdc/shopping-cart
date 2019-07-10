2b04b2045994ebe320664b868db6a020e6523f95
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-10 16:26:30 -0500

    Add catch block to mongoose connection

--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+.then(() => console.log('DB connected'))
+.catch(console.error);
 const connection =  mongoose.connection;
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
