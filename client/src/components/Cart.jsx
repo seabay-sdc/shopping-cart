6b6b3567bd6b49e32b5864666ff00d6c13630da6
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-09 22:12:33 -0500

    Fix total and add Roboto font

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Products from './Products.jsx';
 
 const Cart = ({ cart }) => {
+  const total = cart.reduce((sum, item) => sum += item.price, 0);
   return (
       <div className="container">
         <div className="shopping-cart">
 
           <div className="shopping-cart-header">
             <div className="shopping-cart-total">
-              <span className="lighter-text">Total:</span>
-              <span className="main-color-text">$2,229.97</span>
+              <span className="lighter-text">Total: </span>
+              <span className="main-color-text">${total}</span>
             </div>
           </div>
 
