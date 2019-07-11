cb7b14e2f764153522f07f82268761e08ed84d47
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-11 11:26:25 -0500

    Update DNS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-219-119-132.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-17-4-97.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/seabay.pem',
       ref: 'origin/master',
       repo: 'git@github.com:baebay/shopping-cart.git',
