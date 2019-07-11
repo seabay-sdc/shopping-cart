e153bfbe6cd9279a717291a576467e62f0e8cd20
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-11 13:46:33 -0500

    Add domain to ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-3-17-4-97.us-east-2.compute.amazonaws.com',
+      host: 'seabay.rosscalimlim.me',
       key: '~/.ssh/seabay.pem',
       ref: 'origin/master',
       repo: 'git@github.com:baebay/shopping-cart.git',
