1a0299f0ea324a9f1cfc5cfaef5c096ca5bd6c14
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-15 16:01:40 -0500

    Add material modal test

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from '@material-ui/core/Modal/';
 import Cart from './Cart.jsx';
 import axios from 'axios';
 
--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog/';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 import Products from './Products.jsx';
-import onClickOutside from 'react-onclickoutside';
 
 class Cart extends React.Component {
   constructor (props) {
     super(props);
+
+    this.state = {
+      open: true,
+    };
+
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
-  handleClickOutside (e) {
-    if (e.target.id === "cart") {
-      return;
-    }
-    this.props.toggleMenu();
+  handleClickOpen () {
+    this.setState({ open: true });
+  }
+
+  handleClose () {
+    this.setState({ open: false });
   }
 
   render () {
     const total = this.props.cart.reduce((sum, item) => sum += item.price, 0);
+    // return (
+    //   <div className="container">
+    //     <div className="shopping-cart">
+    //       <div className="shopping-cart-header">
+    //         <div className="shopping-cart-total">
+    //           <span className="lighter-text">Total: </span>
+    //           <span className="main-color-text">${total}</span>
+    //         </div>
+    //       </div>
+    //       <Products cart={this.props.cart} toggleMenu={this.props.toggleMenu} setCurrentItem={this.props.setCurrentItem} />
+    //     </div>
+    //   </div>
+    // );
     return (
-      <div className="container">
-        <div className="shopping-cart">
-          <div className="shopping-cart-header">
-            <div className="shopping-cart-total">
-              <span className="lighter-text">Total: </span>
-              <span className="main-color-text">${total}</span>
-            </div>
-          </div>
-          <Products cart={this.props.cart} toggleMenu={this.props.toggleMenu} setCurrentItem={this.props.setCurrentItem} />
-        </div>
-      </div>
+      <Dialog
+        open={this.state.open}
+        onClose={this.handleClose}
+        scroll='paper'
+      >
+        <DialogTitle>Test Title</DialogTitle>
+      </Dialog>
     );
   }
 };
 
-export default onClickOutside(Cart);
+export default Cart;
