1852d1cdda9c15184c5128bac79bff3b7a8516eb
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-17 21:35:05 -0500

    Fix first test

--- test/server.test.js
+++ test/server.test.js
@@ -1,8 +1,8 @@
 // require expect from chai
 const expect = require('chai').should();
 
-describe('test goes here', () => {
-  it('should put a should test here', () => {
-    // should test here
+describe('First test', () => {
+  it('Should assert true to be true', () => {
+    (true).should.be.true;
   });
 });
