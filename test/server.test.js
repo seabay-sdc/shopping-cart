8e5a06ece635d8468f895670537f73c067c17523
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-17 21:17:43 -0500

    Setup testing skeleton

--- test/server.test.js
+++ test/server.test.js
@@ -1,1 +1,8 @@
+// require expect from chai
+const expect = require('chai').should();
 
+describe('test goes here', () => {
+  it('should put a should test here', () => {
+    // should test here
+  });
+});
