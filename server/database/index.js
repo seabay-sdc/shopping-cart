98c1bafa1c87e0dbd5c1cd9148dc51ed003b495f
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-18 21:47:41 -0500

    Factor out database models

--- server/database/index.js
+++ server/database/index.js
@@ -1,26 +1,4 @@
-const mongoose = require('mongoose');
-const connection = require('./config');
-const Schema = mongoose.Schema;
-
-const ProductSchema = new Schema({
-  id: Number,
-  name: String,
-  price: Number,
-  category: String,
-  img1_url: String
-});
-
-const CartItemSchema = new Schema({
-  id: Number,
-  name: String,
-  price: Number,
-  category: String,
-  img1_url: String,
-  quantity: Number,
-});
-
-const CartItems = connection.model('cart_item', CartItemSchema);
-const Products = connection.model('product', ProductSchema);
+const { CartItems, Products } = require('./models');
 
 // cart methods
 const cart = {
---
+++ server/database/models.js
@@ -0,0 +1,24 @@
+const connection = require('./config');
+const Schema = require('mongoose').Schema;
+
+const ProductSchema = new Schema({
+  id: Number,
+  name: String,
+  price: Number,
+  category: String,
+  img1_url: String
+});
+
+const CartItemSchema = new Schema({
+  id: Number,
+  name: String,
+  price: Number,
+  category: String,
+  img1_url: String,
+  quantity: Number,
+});
+
+const CartItems = connection.model('cart_item', CartItemSchema);
+const Products = connection.model('product', ProductSchema);
+
+module.exports = { CartItems, Products };
