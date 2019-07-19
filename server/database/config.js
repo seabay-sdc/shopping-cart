e182f1a662c964d844b456d36ff8aceb0bd7b729
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-18 22:23:53 -0500

    Remove db connection message that interupts test suite

--- server/database/config.js
+++ server/database/config.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
-  .then(() => console.log('DB connected'))
   .catch(console.error);
 
 const connection =  mongoose.connection;
