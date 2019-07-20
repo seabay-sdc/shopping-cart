868bbe95316530068f7e8a70ff36185d23173436
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-19 19:40:40 -0500

    Refactor cart to be functional

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog/';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -7,13 +7,8 @@ import DialogActions from '@material-ui/core/DialogActions';
 
 import Products from './Products.jsx';
 
-class Cart extends Component {
-  constructor (props) {
-    super(props);
-  }
-  // cart, display, this.props.toggleDisplay, setCurrentItem
-
-  format (number) {
+const Cart = function ({ cart, display, setCurrentItem, toggleDisplay }) {
+  const format = (number) => {
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD'
@@ -21,49 +16,47 @@ class Cart extends Component {
     return formatter.format(number);
   }
 
-  formatTotal () {
-    const total = this.props.cart.reduce((total, item) => {
+  const formatTotal = () => {
+    const total = cart.reduce((total, item) => {
       return total += item.price * item.quantity;
     }, 0);
-    const formattedTotal = this.format(total);
+    const formattedTotal = format(total);
     return formattedTotal;
   }
 
-  render () {
-    return (
-      <Dialog
-        className="shopping-cart"
-        open={this.props.display}
-        fullWidth={true}
-        maxWidth="sm"
-        onClose={this.props.toggleDisplay}
-        scroll='paper'
-      >
-        <DialogTitle>
-          <div>Cart</div>
-        </DialogTitle>
-        <DialogContent>
-          <Products
-            cart={this.props.cart}
-            format={this.format}
-            toggleDisplay={this.props.toggleDisplay}
-            setCurrentItem={this.props.setCurrentItem}
-          />
-          <div className="shopping-cart-total">
-            Subtotal: {this.formatTotal()}
-          </div>
-        </DialogContent>
-        <DialogActions>
-            <Button onClick={this.props.toggleDisplay} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.props.toggleDisplay} color="primary" variant="contained">
-              Proceed to Checkout
-            </Button>
-          </DialogActions>
-      </Dialog>
-    );
-  }
+  return (
+    <Dialog
+      className="shopping-cart"
+      open={display}
+      fullWidth={true}
+      maxWidth="sm"
+      onClose={toggleDisplay}
+      scroll='paper'
+    >
+      <DialogTitle>
+        <div>Cart</div>
+      </DialogTitle>
+      <DialogContent>
+        <Products
+          cart={cart}
+          format={format}
+          toggleDisplay={toggleDisplay}
+          setCurrentItem={setCurrentItem}
+        />
+        <div className="shopping-cart-total">
+          Subtotal: {formatTotal()}
+        </div>
+      </DialogContent>
+      <DialogActions>
+          <Button onClick={toggleDisplay} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={toggleDisplay} color="primary" variant="contained">
+            Proceed to Checkout
+          </Button>
+        </DialogActions>
+    </Dialog>
+  );
 }
 
 export default Cart;
