ffe755fb3dbcdc819cdc589075bae7f497ef68cf
Landon <landon.baker@utexas.edu>
2019-07-27 16:05:19 -0500

    change table name in seed file

--- test/data/db-init.sh
+++ test/data/db-init.sh
@@ -2,10 +2,10 @@
 
 echo 'Seeding the test database'
 
-psql -d productsTEST -c "INSERT INTO productsTest (prodid, name, price, category, img1_url) VALUES ('abc123xyz', 'Delicious Frozen Carpet', 16.99, 'Home Goods', 'picturesoffrozencarpet.com/pic1');"
-psql -d productsTEST -c "INSERT INTO productsTest (prodid, name, price, category, img1_url) VALUES ('gha583kax', 'Fresh Cool Potatoes', 59.31, 'Jewelry', 'coolpotatoes.net/pics');"
-psql -d productsTEST -c "INSERT INTO productsTest (prodid, name, price, category, img1_url) VALUES ('wjq831xol', 'Neat Rusty Spoons', 18.79, 'Automotive', 'spoons.gov/pics');"
-psql -d productsTEST -c "INSERT INTO productsTest (prodid, name, price, category, img1_url) VALUES ('qox839utj', 'Flaky Retired Meat', 82.14, 'Lighting', 'meatmeatmeat.com/meat');"
+psql -d productsTEST -c "INSERT INTO productsTEST (prodid, name, price, category, img1_url) VALUES ('abc123xyz', 'Delicious Frozen Carpet', 16.99, 'Home Goods', 'picturesoffrozencarpet.com/pic1');"
+psql -d productsTEST -c "INSERT INTO productsTEST (prodid, name, price, category, img1_url) VALUES ('gha583kax', 'Fresh Cool Potatoes', 59.31, 'Jewelry', 'coolpotatoes.net/pics');"
+psql -d productsTEST -c "INSERT INTO productsTEST (prodid, name, price, category, img1_url) VALUES ('wjq831xol', 'Neat Rusty Spoons', 18.79, 'Automotive', 'spoons.gov/pics');"
+psql -d productsTEST -c "INSERT INTO productsTEST (prodid, name, price, category, img1_url) VALUES ('qox839utj', 'Flaky Retired Meat', 82.14, 'Lighting', 'meatmeatmeat.com/meat');"
 
 psql -d productsTEST -c "INSERT INTO cartItems (prodid, name, price, category, img1_url, quantity) VALUES ('abc123xyz', 'Delicious Frozen Carpet', 16.99, 'Home Goods', 'picturesoffrozencarpet.com/pic1', 6);"
 psql -d productsTEST -c "INSERT INTO cartItems (prodid, name, price, category, img1_url, quantity) VALUES ('gha583kax', 'Fresh Cool Potatoes', 59.31, 'Jewelry', 'coolpotatoes.net/pics', 4);"
