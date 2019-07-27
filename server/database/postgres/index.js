2e91d49600c27dc94aba9e9f376af78953c96b5f
Landon <landon.baker@utexas.edu>
2019-07-26 21:52:56 -0500

    refactor and test SQL cart db methods

--- server/database/postgres/index.js
+++ server/database/postgres/index.js
@@ -3,24 +3,51 @@ const client = require('./config');
 // cart methods
 const cart = {
   get: async (item = {}) => {
-    let query = await client.query('SELECT * FROM cartItems');
-    console.log(query.rows);
-    return query.rows;
+    let query;
+    item.id
+      ? (query = await client.query(
+          'SELECT * FROM cartItems WHERE prodid = $1',
+          [item.id]
+        ))
+      : (query = await client.query('SELECT * FROM cartItems'));
+    return new Promise((resolve, reject) => {
+      resolve(query.rows);
+    });
   },
-  add: async item => {
-    const [product] = await products.get({ id: item.id });
-    const cartQuery = await cart.get({ id: product.id });
-    const cartItem = { ...product._doc, quantity: item.quantity };
 
-    return cartQuery.length === 0
-      ? CartItems.create(cartItem)
-      : CartItems.updateOne(
-          { id: product.id },
-          { $inc: { quantity: item.quantity } }
-        );
+  add: async item => {
+    let productQuery = await client.query(
+      'SELECT * FROM productsTEST WHERE prodid = $1',
+      [item.id]
+    );
+    const product = productQuery.rows[0];
+    const cartQuery = await cart.get({ id: product.prodid });
+    const cartItem = { ...product, quantity: item.quantity };
+
+    if (cartQuery.length === 0) {
+      await client.query(
+        'INSERT INTO cartItems (prodid, name, price, category, img1_url, quantity) VALUES ($1, $2, $3, $4, $5, $6)',
+        [
+          cartItem.prodid,
+          cartItem.name,
+          cartItem.price,
+          cartItem.category,
+          cartItem.img1_url,
+          cartItem.quantity
+        ]
+      );
+    } else {
+      await client.query(
+        'UPDATE cartItems SET quantity = quantity + 1 WHERE prodid = $1',
+        [cartItem.prodid]
+      );
+    }
   },
-  remove: item => {
-    return CartItems.deleteOne(item).exec();
+  remove: async item => {
+    await client.query('DELETE FROM cartItems WHERE prodid = $1', [item.id]);
+  },
+  end: () => {
+    client.end();
   }
 };
 
--- test/pg.test.js
+++ test/pg.test.js
@@ -1,11 +1,99 @@
 const db = require('../server/database/postgres');
 const { expect } = require('chai');
 
-describe('CART METHODS', () => {
+describe('Postgres Cart Methods', () => {
+  after(async () => {
+    await db.cart.end();
+  });
   describe('cart.get()', () => {
-    it('should get all of the items in the cart', async () => {
-      const cartItems = await db.cart.get();
+    let cartItems;
+
+    before(async () => {
+      cartItems = await db.cart.get();
+    });
+
+    it('returns an array', () => {
       expect(cartItems).to.be.an.instanceOf(Array);
+    });
+    it('returns all cart items when query is blank', () => {
+      expect(cartItems).to.have.length(2);
+    });
+    it('returns specific item if query is not blank', async () => {
+      let singleItem = await db.cart.get({ id: 'gha583kax' });
+      expect(singleItem).to.have.length(1);
+      expect(singleItem[0]).to.haveOwnProperty('price');
+    });
+    it('has the expected properties', () => {
+      expect(cartItems[0]).to.haveOwnProperty('id');
+      expect(cartItems[0]).to.haveOwnProperty('prodid');
+      expect(cartItems[0]).to.haveOwnProperty('name');
+      expect(cartItems[0]).to.haveOwnProperty('price');
+      expect(cartItems[0]).to.haveOwnProperty('category');
+      expect(cartItems[0]).to.haveOwnProperty('img1_url');
+      expect(cartItems[0]).to.haveOwnProperty('quantity');
+    });
+    it('returns data in the appropriate shape', () => {
+      expect(typeof cartItems[0]['id']).to.equal('number');
+      expect(typeof cartItems[0]['prodid']).to.equal('string');
+      expect(typeof cartItems[0]['name']).to.equal('string');
+      expect(typeof cartItems[0]['price']).to.equal('string');
+      expect(typeof cartItems[0]['category']).to.equal('string');
+      expect(typeof cartItems[0]['img1_url']).to.equal('string');
+      expect(typeof cartItems[0]['quantity']).to.equal('number');
+    });
+  });
+
+  describe('cart.add()', () => {
+    let productBefore;
+    let productAfter;
+
+    before(async () => {
+      productBefore = await db.cart.get({ id: 'abc123xyz' });
+      await db.cart.add({ id: 'abc123xyz' });
+      productAfter = await db.cart.get({ id: 'abc123xyz' });
+    });
+
+    after(async () => {
+      await db.cart.remove({ id: 'wjq831xol' });
+    });
+
+    it('increments the quantity if the item is in the cart', () => {
+      expect(productAfter[0].quantity).to.equal(productBefore[0].quantity + 1);
+    });
+    it('adds a new product to the cart', async () => {
+      let cartBefore = await db.cart.get();
+      await db.cart.add({ id: 'wjq831xol', quantity: 6 });
+      let cartAfter = await db.cart.get();
+      cartBefore = cartBefore.filter(item => item.prodid === 'wjq831xol');
+      cartAfter = cartAfter.filter(item => item.prodid === 'wjq831xol');
+      expect(cartBefore).to.have.length(0);
+      expect(cartAfter).to.have.length(1);
+    });
+  });
+
+  describe('cart.remove()', () => {
+    before(async () => {
+      await db.cart.add({ id: 'wjq831xol' });
+    });
+    after(async () => {
+      await db.cart.remove({ id: 'wjq831xol' });
+    });
+
+    it('removes the item from the cart', async () => {
+      const itemBefore = await db.cart.get({ id: 'wjq831xol' });
+      await db.cart.remove({ id: 'wjq831xol' });
+      const itemAfter = db.cart.get({ id: 'wjq831xol' });
+      expect(itemBefore[0]).to.be.ok;
+      expect(itemAfter[0]).to.be.undefined;
+    });
+    it('adds a new product to the cart', async () => {
+      let cartBefore = await db.cart.get();
+      await db.cart.add({ id: 'wjq831xol', quantity: 6 });
+      let cartAfter = await db.cart.get();
+      cartBefore = cartBefore.filter(item => item.prodid === 'wjq831xol');
+      cartAfter = cartAfter.filter(item => item.prodid === 'wjq831xol');
+      expect(cartBefore).to.have.length(0);
+      expect(cartAfter).to.have.length(1);
     });
   });
 });
