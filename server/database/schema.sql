f5afad1a5d70fa0313e892752e8575e3a8dd88dc
Landon <landon.baker@utexas.edu>
2019-07-31 12:09:35 -0500

    refactor schema.sql for use in remote server

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,8 +1,26 @@
-CREATE TABLE productsTEST (
-  id SERIAL PRIMARY KEY,
-  prodid VARCHAR(45),
+DROP DATABASE IF EXISTS sdc;
+
+CREATE DATABASE sdc;
+
+\c sdc;
+
+CREATE TABLE products (
+  id SERIAL,
+  prodid VARCHAR(45) PRIMARY KEY,
   name VARCHAR(45),
   price DECIMAL(7,2),
   category VARCHAR(45),
   img1_url VARCHAR(45)
 );
+
+CREATE TABLE cartItems (
+  id SERIAL PRIMARY KEY,
+  prodid VARCHAR(45),
+  name VARCHAR(45),
+  price DECIMAL(7,2),
+  category VARCHAR(45),
+  img1_url VARCHAR(45),
+  quantity INT
+);
+
+\copy testTable (proid, name, price, category, img1_url) FROM './seeding.csv' DELIMITER ',' CSV HEADER;
