a4e1b9e998dc2fa576428dfc89ce7f9f83737184
Landon <landon.baker@utexas.edu>
2019-08-01 19:56:11 -0500

    prettier formatting

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,14 +8,20 @@ import Badge from '@material-ui/core/Badge';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import AttachMoney from '@material-ui/icons/AttachMoney'
-import Notifications from '@material-ui/icons/Notifications'
-import AccountCircle from '@material-ui/icons/AccountCircle'
+import AttachMoney from '@material-ui/icons/AttachMoney';
+import Notifications from '@material-ui/icons/Notifications';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
 import MenuIcon from '@material-ui/icons/Menu';
 import axios from 'axios';
 import MediaQuery from 'react-responsive';
-import { createMuiTheme, MuiThemeProvider, Divider, List, ListItem} from "@material-ui/core";
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  Divider,
+  List,
+  ListItem
+} from '@material-ui/core';
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 import Drawer from '@material-ui/core/Drawer';
 import ListSubheader from '@material-ui/core/ListSubheader';
@@ -26,30 +32,28 @@ import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import InputIcon from '@material-ui/icons/Input';
 import HelpIcon from '@material-ui/icons/Help';
 
-
 const host = process.env.API_HOST;
 const port = process.env.API_PORT;
 const uri = `http://${host}:${port}`;
 
 const styles = {
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   spacer: {
-    flexGrow: 1,
+    flexGrow: 1
   },
-  menuButton : {
-    marginLeft : -10,
-    marginRight : -10,
-  },
-
+  menuButton: {
+    marginLeft: -10,
+    marginRight: -10
+  }
 };
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#0065D2"
-    },
+      main: '#0065D2'
+    }
     //left secondary color unstyled so notification would stay red
     // secondary: {
     //   main: "#009AF7"
@@ -63,7 +67,7 @@ const generateClassName = createGenerateClassName({
 });
 
 class NavBar extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       cart: [],
@@ -74,41 +78,43 @@ class NavBar extends Component {
     this.handleMenuToggle = this.handleMenuToggle.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     document.addEventListener('addToCart', ({ detail }) => {
-      axios.post(`${uri}/api/cart/items/`, detail)
-      .then(() => this.getCartItems())
-      .catch(console.error);
+      axios
+        .post(`${uri}/api/cart/items/`, detail)
+        .then(() => this.getCartItems())
+        .catch(console.error);
     });
     this.getCartItems();
   }
 
-  getCartItems () {
-    axios.get(`${uri}/api/cart/items`)
-    .then(({ data }) => this.setState({ cart: data }))
-    .catch(console.error);
+  getCartItems() {
+    axios
+      .get(`${uri}/api/cart/items`)
+      .then(({ data }) => this.setState({ cart: data }))
+      .catch(console.error);
   }
 
-  setCurrentItem (id) {
+  setCurrentItem(id) {
     const detail = { detail: { id } };
     const event = new CustomEvent('setCurrentItem', detail);
     document.dispatchEvent(event);
   }
 
-  toggleDisplay () {
+  toggleDisplay() {
     this.setState({ display: !this.state.display });
   }
 
   handleMenuToggle() {
-    this.setState({drawerOpen : !(this.state.drawerOpen)})
+    this.setState({ drawerOpen: !this.state.drawerOpen });
   }
 
-  render () {
-    const {classes} = this.props;
+  render() {
+    const { classes } = this.props;
 
     const responsiveCart = (
       <>
-        <MediaQuery query='(max-width: 600px)'>
+        <MediaQuery query="(max-width: 600px)">
           <CartDrawer
             cart={this.state.cart}
             display={this.state.display}
@@ -116,20 +122,18 @@ class NavBar extends Component {
             toggleDisplay={this.toggleDisplay}
           />
         </MediaQuery>
-        <MediaQuery query='(min-width: 601px)'>
+        <MediaQuery query="(min-width: 601px)">
           <Cart
-          cart={this.state.cart}
-          display={this.state.display}
-          setCurrentItem={this.setCurrentItem}
-          toggleDisplay={this.toggleDisplay}
+            cart={this.state.cart}
+            display={this.state.display}
+            setCurrentItem={this.setCurrentItem}
+            toggleDisplay={this.toggleDisplay}
           />
         </MediaQuery>
       </>
     );
 
-    const cartDisplay = this.state.display
-    ? responsiveCart
-    : null;
+    const cartDisplay = this.state.display ? responsiveCart : null;
 
     return (
       <StylesProvider generateClassName={generateClassName}>
@@ -137,34 +141,48 @@ class NavBar extends Component {
           <div className={this.props.classes.root}>
             <AppBar position="static">
               <Toolbar>
+                <MediaQuery query="(max-width: 600px)">
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="Menu"
+                    onClick={this.handleMenuToggle}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                </MediaQuery>
 
-              <MediaQuery query='(max-width: 600px)'>
-                <IconButton 
-                  edge="start" 
-                  color="inherit" 
-                  aria-label="Menu" 
-                  onClick={this.handleMenuToggle}>
-                  <MenuIcon/>
-                </IconButton>
-              </MediaQuery>
-
-              <MediaQuery query='(min-width: 601px)'>
-                <Button className={classes.menuButton} color="inherit" size="large">
-                  Login
-                </Button>
-                <Button className={classes.menuButton} color="inherit" size="large">
-                  Deals
-                </Button>
-                <Button className={classes.menuButton} color="inherit" size="large">
-                  Gift Cards
-                </Button>
-                <Button className={classes.menuButton} color="inherit" size="large">
-                  Help
-                </Button>
-              </MediaQuery>
+                <MediaQuery query="(min-width: 601px)">
+                  <Button
+                    className={classes.menuButton}
+                    color="inherit"
+                    size="large"
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    className={classes.menuButton}
+                    color="inherit"
+                    size="large"
+                  >
+                    Deals
+                  </Button>
+                  <Button
+                    className={classes.menuButton}
+                    color="inherit"
+                    size="large"
+                  >
+                    Gift Cards
+                  </Button>
+                  <Button
+                    className={classes.menuButton}
+                    color="inherit"
+                    size="large"
+                  >
+                    Help
+                  </Button>
+                </MediaQuery>
                 <Typography className={this.props.classes.spacer}></Typography>
-
-
 
                 <IconButton color="inherit" aria-label="Sell">
                   <AttachMoney />
@@ -175,17 +193,22 @@ class NavBar extends Component {
                 <IconButton color="inherit" aria-label="Notifications">
                   <Notifications />
                 </IconButton>
-                <IconButton color="inherit" aria-label="Cart" onClick={this.toggleDisplay}>
-                  <Badge badgeContent={this.state.cart.length} color="secondary">
+                <IconButton
+                  color="inherit"
+                  aria-label="Cart"
+                  onClick={this.toggleDisplay}
+                >
+                  <Badge
+                    badgeContent={this.state.cart.length}
+                    color="secondary"
+                  >
                     <ShoppingCart />
                   </Badge>
                 </IconButton>
               </Toolbar>
             </AppBar>
 
-            <Drawer
-              docked={false}
-              open={this.state.drawerOpen}>
+            <Drawer docked={false} open={this.state.drawerOpen}>
               {/* <IconButton 
                 edge="start" 
                 color="inherit" 
@@ -195,51 +218,46 @@ class NavBar extends Component {
                 <MenuIcon/>
               </IconButton>
               Hello asdfasdfa sdfas dfasdf asdf asd fasdf asdf */}
-             <AppBar position="static" style={{width : 200}}>
-              <Toolbar>
-                <IconButton 
-                  edge="start" 
-                  color="inherit" 
-                  aria-label="Menu" 
-                  onClick={this.handleMenuToggle}>
-                  <MenuIcon/>
-                </IconButton>
-              </Toolbar>
-            </AppBar>
-             {/* <Divider/> */}
-             <List>
-               <ListItem button>
+              <AppBar position="static" style={{ width: 200 }}>
+                <Toolbar>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="Menu"
+                    onClick={this.handleMenuToggle}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                </Toolbar>
+              </AppBar>
+              {/* <Divider/> */}
+              <List>
+                <ListItem button>
                   <ListItemIcon>
                     <InputIcon />
                   </ListItemIcon>
                   <ListItemText primary="Login" />
-               </ListItem>
-               <ListItem button>
-
-                 <ListItemIcon>
-                   <AttachMoneyIcon />
-                 </ListItemIcon>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <AttachMoneyIcon />
+                  </ListItemIcon>
                   <ListItemText primary="Deals" />
-               </ListItem>
-               <ListItem button>
-
-                 <ListItemIcon>
-                   <CardGiftcardIcon />
-                 </ListItemIcon>
-                <ListItemText primary="Gift Cards" />
-               </ListItem>
-               <ListItem button>
-                 <ListItemIcon>
-                   <HelpIcon />
-                 </ListItemIcon>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <CardGiftcardIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Gift Cards" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                    <HelpIcon />
+                  </ListItemIcon>
                   <ListItemText primary="Help" />
-               </ListItem>
-
-             </List>
+                </ListItem>
+              </List>
             </Drawer>
-
-
-
 
             {cartDisplay}
           </div>
@@ -247,6 +265,6 @@ class NavBar extends Component {
       </StylesProvider>
     );
   }
-};
+}
 
 export default withStyles(styles)(NavBar);
